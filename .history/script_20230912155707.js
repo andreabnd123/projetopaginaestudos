d22685d17b5a6ca14b423532bbf509912babea51
andreabnd123 <andreabnd123@gmail.com>
2023-09-12 15:57:35 -0300

    alterações

---
+++ .history/script_20230912155707.js
@@ -0,0 +1,52 @@
+// script.js
+function validarFormularioContato() {
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    if (nome === "" || email === "" || mensagem === "") {
+        alert("Por favor, preencha todos os campos do formulário.");
+        return false;
+    }
+
+    alert("Mensagem enviada com sucesso!");
+    return true;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const quizForm = document.getElementById("quiz-form");
+    const resultContainer = document.getElementById("result-container");
+    const resultText = document.getElementById("result-text");
+
+    quizForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const respostas = {
+            pergunta1: document.querySelector('input[name="pergunta1"]:checked'),
+            pergunta2: document.querySelector('input[name="pergunta2"]:checked'),
+            pergunta3: document.querySelector('input[name="pergunta3"]:checked')
+        };
+
+        let pontuacao = 0;
+
+        if (respostas.pergunta1 && respostas.pergunta1.value === "paris") {
+            pontuacao++;
+        }
+        if (respostas.pergunta2 && respostas.pergunta2.value === "jupiter") {
+            pontuacao++;
+        }
+        if (respostas.pergunta3 && respostas.pergunta3.value === "azul") {
+            pontuacao++;
+        }
+
+        resultText.textContent = `Você acertou ${pontuacao} de 3 perguntas.`;
+        resultContainer.style.display = "block";
+    });
+});
+
+
+<form id="search-form">
+    <input type="text" id="search-input" placeholder="Pesquisar...">
+    <button type="submit">Buscar</button>
+</form>
+
--- script.js
+++ script.js
@@ -13,67 +13,36 @@ function validarFormularioContato() {
     return true;
 }
 
-// script.js
-const questions = [
-    {
-        question: "Qual é a capital do Brasil?",
-        options: ["Rio de Janeiro", "Brasília", "São Paulo", "Salvador"],
-        answer: "Brasília"
-    },
-    {
-        question: "Qual é o maior planeta do sistema solar?",
-        options: ["Terra", "Júpiter", "Marte", "Vênus"],
-        answer: "Júpiter"
-    },
-    // Adicione mais perguntas aqui
-];
-
-let currentQuestion = 0;
-let score = 0;
-
-function loadQuestion() {
-    const questionText = document.getElementById("question-text");
-    const optionsContainer = document.getElementById("options-container");
-
-    questionText.textContent = questions[currentQuestion].question;
-    optionsContainer.innerHTML = "";
-
-    for (let i = 0; i < questions[currentQuestion].options.length; i++) {
-        const option = document.createElement("button");
-        option.textContent = questions[currentQuestion].options[i];
-        option.classList.add("option");
-        option.addEventListener("click", function () {
-            checkAnswer(this);
-        });
-        optionsContainer.appendChild(option);
-    }
-}
-
-function checkAnswer(selectedOption) {
-    if (selectedOption.textContent === questions[currentQuestion].answer) {
-        score++;
-    }
-
-    currentQuestion++;
-
-    if (currentQuestion < questions.length) {
-        loadQuestion();
-    } else {
-        showResult();
-    }
-}
-
-function showResult() {
-    const quizContainer = document.getElementById("quiz-container");
+document.addEventListener("DOMContentLoaded", function () {
+    const quizForm = document.getElementById("quiz-form");
     const resultContainer = document.getElementById("result-container");
     const resultText = document.getElementById("result-text");
 
-    quizContainer.style.display = "none";
-    resultContainer.style.display = "block";
-    resultText.textContent = `Você acertou ${score} de ${questions.length} perguntas.`;
-}
+    quizForm.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-loadQuestion();
+        const respostas = {
+            pergunta1: document.querySelector('input[name="pergunta1"]:checked'),
+            pergunta2: document.querySelector('input[name="pergunta2"]:checked'),
+            pergunta3: document.querySelector('input[name="pergunta3"]:checked')
+        };
+
+        let pontuacao = 0;
+
+        if (respostas.pergunta1 && respostas.pergunta1.value === "paris") {
+            pontuacao++;
+        }
+        if (respostas.pergunta2 && respostas.pergunta2.value === "jupiter") {
+            pontuacao++;
+        }
+        if (respostas.pergunta3 && respostas.pergunta3.value === "azul") {
+            pontuacao++;
+        }
+
+        resultText.textContent = `Você acertou ${pontuacao} de 3 perguntas.`;
+        resultContainer.style.display = "block";
+    });
+});
 
 <form id="search-form">
     <input type="text" id="search-input" placeholder="Pesquisar...">
